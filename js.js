5e69e1c636ff775663a5a8ee8f78ebd9f2d845fb
user <d1204433@o365.fcu.edu.tw>
2024-06-08 00:15:15 +0800

    v1.2

---
+++ js.js
@@ -0,0 +1,12 @@
+document.getElementById('hov1').addEventListener('mouseover', function() {
+    document.querySelector('.hover1').style.display = 'block';
+    document.querySelector('.hover2').style.display = 'block';
+    document.querySelector('.hover3').style.display = 'block';
+    document.querySelector('.hover4').style.display = 'block';
+    document.querySelector('.hover5').style.display = 'block';
+});
+document.getElementById('hov2').addEventListener('mouseover',function()
+    {
+        document.querySelector('.hover6').style.display = 'block';
+    }
+);
